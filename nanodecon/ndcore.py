630a8a51546843cdd51d5e9cdb2853480593166b
MBHallgren <s153002@student.dtu.dk>
2022-11-01 15:26:35 +0100

    push10

--- nanodecon/ndcore.py
+++ nanodecon/ndcore.py
@@ -22,10 +22,35 @@ def nano_decon(args):
         raise
     return True
 
+class Kma_result:
+    def __init__(self, line):
+        self.line = line
+        self.line = self.line.split("\t")
+        self.name = self.line[0]
+        self.score = float(self.line[1])
+        self.expected_score = float(self.line[2])
+        self.template_length = float(self.line[3])
+        self.template_identity = float(self.line[4])
+        self.template_coverage = float(self.line[5])
+        self.query_identity = float(self.line[6])
+        self.query_coverage = float(self.line[7])
+        self.depth = float(self.line[8])
+
 def primary_search(args):
     logging.info("Primary search")
     cmd = "kma -i {}/trimmed-reads.fastq.gz -o {}/primary-search -t_db {} -t 8 -mem_mode -mp 20 -mrs 0.0 -bcNano -bc 0.7".format(args.output, args.output, args.bac_db)
     os.system(cmd)
+    cmd = "sort -t$'\t' -k2nr {}/primary-search.res > {}/primary-search.sorted.res".format(args.output, args.output)
+    os.system(cmd)
+
+    #Read in primary search results sorted
+    with open("{}/primary-search.sorted.res".format(args.output), "r") as f:
+        f.readline()
+        line = f.readline()
+        top_scoring_template = Kma_result(line)
+    print (top_scoring_template.name)
+
+
 
 def set_up_output_folder(args):
     if not os.path.exists(args.output):
